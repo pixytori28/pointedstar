f8bc0aa399c4d7d413b35fcbd6bf2e9664441601
pixytori28 <victoria.inahuazo11@myhunter.cuny.edu>
2024-02-21 16:42:24 -0500

    Add files via upload

---
+++ #5pointedstar.py
@@ -0,0 +1,13 @@
+#5pointedstar
+
+#Victoria Inahuazo
+#victoria.inahuazo11@myhunter.cuny.edu
+#Date:02/21/24
+
+import turtle 
+star = turtle.Turtle()
+
+for i in range (5):
+    star.forward(100)
+    star.left(144)
+
